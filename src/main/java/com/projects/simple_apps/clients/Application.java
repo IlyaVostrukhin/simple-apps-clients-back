fbad9b8952871e48939d4cc11c850c6b822cddf8
Ilya Vostrukhin <GuitaristNSK@yandex.ru>
2022-06-24 16:27:22 +0700

    packaging - war

--- src/main/java/com/projects/simple_apps/clients/Application.java
+++ src/main/java/com/projects/simple_apps/clients/Application.java
@@ -24,6 +24,9 @@ package com.projects.simple_apps.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -32,7 +35,10 @@ import org.springframework.context.annotation.Configuration;
 //@Configuration
 //@EnableAutoConfiguration
 //@ComponentScan(basePackages = "com.projects.simple_apps.clients")
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
