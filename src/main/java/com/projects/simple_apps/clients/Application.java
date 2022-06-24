0677604c462e0f7cfc8211fb82dcc518a82e4534
Ilya Vostrukhin <GuitaristNSK@yandex.ru>
2022-06-24 13:52:34 +0700

    packaging - war

--- src/main/java/com/projects/simple_apps/clients/Application.java
+++ src/main/java/com/projects/simple_apps/clients/Application.java
@@ -7,12 +7,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        System.out.print("Starting app with System Args: [" );
-        for (String s : args) {
-            System.out.print(s + " ");
-        }
-        System.out.println("]");
-        app.run(args);
+        SpringApplication.run(Application.class);
+//        SpringApplication app = new SpringApplication(Application.class);
+//        System.out.print("Starting app with System Args: [" );
+//        for (String s : args) {
+//            System.out.print(s + " ");
+//        }
+//        System.out.println("]");
+//        app.run(args);
     }
 }
