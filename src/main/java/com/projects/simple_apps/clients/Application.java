82f0f8450392b57c966e93106b496d377246f82f
Ilya Vostrukhin <GuitaristNSK@yandex.ru>
2022-06-24 18:09:39 +0700

    packaging - war

--- src/main/java/com/projects/simple_apps/clients/Application.java
+++ src/main/java/com/projects/simple_apps/clients/Application.java
@@ -1,34 +1,13 @@
-//package com.projects.simple_apps.clients;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-//import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.context.annotation.Configuration;
-//
-//@SpringBootApplication(exclude = {
-//        DataSourceAutoConfiguration.class,
-//        DataSourceTransactionManagerAutoConfiguration.class,
-//        HibernateJpaAutoConfiguration.class})
-//@Configuration
-//public class Application extends SpringBootServletInitializer {
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class);
-//    }
-//}
-
 package com.projects.simple_apps.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication()
-@EnableJpaRepositories(value = "com.projects.simple_apps.clients.repository")
+//@SpringBootApplication()
+//@EnableJpaRepositories(value = "com.projects.simple_apps.clients.repository")
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
