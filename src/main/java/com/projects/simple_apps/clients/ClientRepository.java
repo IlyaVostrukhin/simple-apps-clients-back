5b9cc8cd4d34fb627f3b9ea6d1c2d9ea78b3c5ef
Ilya Vostrukhin <GuitaristNSK@yandex.ru>
2022-06-24 14:33:15 +0700

    packaging - war

--- src/main/java/com/projects/simple_apps/clients/ClientRepository.java
+++ src/main/java/com/projects/simple_apps/clients/ClientRepository.java
@@ -1,6 +1,8 @@
 package com.projects.simple_apps.clients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
